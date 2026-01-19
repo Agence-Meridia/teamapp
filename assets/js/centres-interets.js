ef7ed31ac1f9fb13757cf7536669ae81980f992b
Agence-Meridia <bernie.girault@gmail.com>
2026-01-19 17:04:17 +0100

    Add files via upload

--- assets/js/centres-interets.js
+++ assets/js/centres-interets.js
@@ -1,1 +1,17 @@
-
+const centresInterets = [
+  {
+    categorie: "Sport",
+    items: [
+      { id: 1, label: "Football" },
+      { id: 2, label: "Basket" },
+      { id: 3, label: "Padel" }
+    ]
+  },
+  {
+    categorie: "Loisirs",
+    items: [
+      { id: 20, label: "Cinéma" },
+      { id: 21, label: "Jeux vidéo" }
+    ]
+  }
+];
